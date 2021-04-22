46e90d4f095f833a000b46dd9509e795c10a8dd7
Jachym Jaluvka <j.jaluvka@liverpool.ac.uk>
2021-04-22 15:08:46 +0100

    Added Database connection object and updated ser class

---
+++ models/DBHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class DBHandler {
+    private $db;
+
+    function __construct() {
+        $this->connect_database();
+    }
+
+    public function getInstance() {
+        return $this->db;
+    }
+
+    private function connect_database() {
+        define('USER', 'user');
+        define('PASSWORD', 'password');
+
+        // Database connection
+        try {
+            $connection_string = 'mysql:host=localhost;dbname=mydb;charset=utf8';
+            $connection_array = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            );
+
+            $this->db = new PDO($connection_string, USER, PASSWORD, $connection_array);
+            echo 'Database connection established';
+        }
+        catch(PDOException $e) {
+            $this->db = null;
+        }
+    }   
+}
--- models/User.php
+++ models/User.php
@@ -1,1 +1,63 @@
-<!-- TODO: php classes -->
+<?php
+
+    require_once("DBHandler.php");
+
+    class User
+    {
+        public $username;
+        public $firstName;
+        public $lastName;
+        public $email;
+        public $id;
+        public $admin;
+        public $authenticated = false;
+
+        public $aiObject;
+
+        public function __construct($firstName="",$lastName="",$email="",$admin=false){
+            $this->firstName = $firstName;
+            $this->lastName = $lastName;
+            $this->email = $email;
+            $this->admin = $admin;
+
+            //save new user and retrieve id for that user
+            if($this->firstName != ""){
+
+                $this->authenticated = true;
+
+            }
+        }
+
+        //authenticate users credentials
+        public function authenticate($pwd){
+            $valid = false;
+
+            //query db and check pwd
+            
+
+            if($valid){
+                $this->authenticated = true;
+            }
+
+            return $valid;           
+
+        }
+
+        //find user by unique username in the db
+        public function find_user($username){
+
+        }
+
+        public function predict(){
+
+        }
+
+        public function rate_recipe($recipe_id,$rating){
+
+        }
+
+
+
+
+    }
+ 
