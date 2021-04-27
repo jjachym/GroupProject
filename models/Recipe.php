982b7d9cf48e39e5956e024b9c5320425b93d001
Jachym Jaluvka <j.jaluvka@liverpool.ac.uk>
2021-04-27 19:21:52 +0100

    Added methods to Recipe class

--- models/Recipe.php
+++ models/Recipe.php
@@ -20,8 +20,32 @@
             $this->tags = $tags;
         }
 
-        public static function find_recipe($title){
+        //static methods called using Recipe::function_name()
 
+        /**static method that fetches all recipes(maybe limit to 200?) and returns an array of Recipe objects
+         * 
+         * optional argument options could be used for slight filtering, i.e. all italian recipes etc.
+         * */
+        public static function fetchAll($options=null){
+
+            //return array of recipe objects
+
+        }
+
+
+        //method for finding a single recipe by title
+        public function findRecipe($title){
+
+            //populate current Recipe objects attributes with data found in db
+
+            //return true if found
+        }
+
+        //method that save a Recipe object into the db
+        public function save(){
+
+            //return true if insertion into db is valid
+            
         }
 
 
