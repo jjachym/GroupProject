9b4dd6ab43c5b8a2c2a0dff1f57e6c0c324c0d2d
Denis <denis.usov0309@gmail.com>
2021-05-04 00:17:08 +0100

    made index page

--- index.php
+++ index.php
@@ -1,9 +1,43 @@
 <!DOCTYPE html>
 <html>
-    <body>
 
-    <h1>Index Page</h1>
-    <p>add something here</p>
+<body>
+    <iframe src="Master.html" width = "100%" height = "72" style="border:none;"></iframe>
+    
+    <div>
+        <h1>Welcome to our site!</h1>
+    </div>
 
-    </body>
+    <div>
+        <p>
+            Welcome to AI Foods!
+
+        </p>
+
+        <p>
+            -The only site you need to cook a bite!
+        </p>
+
+        <p>
+            Through the AI foods site, you can create your personal account to search through hundreds of recipes right at your fingertips! Ingredient lists, own recipe adding, 
+            ratings - we cater for all. Wether you’re a student on a budget, a novice or veteran chef - AI foods provide the recipes.
+        </p>
+
+        <p>
+            If you would like to search a recipe head over to our search recipe page where you can perform a search based on
+            based on particular ingridients, the name of a recipe, or combine the two to really tailor your culinary query!
+        </p>
+
+        <p>
+            Have a recipe that is too good not to share? Head over to our add recipe page where you can submit your own recipe! Add a recipe
+            name, description, ingridients, instructions, and tags. Press submit and let the world know about your recipe!
+        </p>
+
+        <p>
+            If you’re new here, click sign-up to create your account or if you’re already with us, hit the login button to get started. 
+            Welcome to your latest and greatest recipe book, AI foods.
+        </p>
+    </div>
+
+</body>
 </html>
